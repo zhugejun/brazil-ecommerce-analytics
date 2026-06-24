f5f6b20d75b553f29cede4cc71a85132b0e7f90b
Gejun Zhu <gejun.anderson@gmail.com>
2026-06-23 22:17:01 -0500

    add product category translation

--- brazil_ecommerce_analytics/models/staging/stg_olist__category_name_translation.sql
+++ brazil_ecommerce_analytics/models/staging/stg_olist__category_name_translation.sql
@@ -4,8 +4,8 @@ with source as (
 
 renamed as (
   select
-    category_name as category_name_portuguese,
-    category_name_english as category_name
+    product_category_name as product_category_name_portuguese,
+    product_category_name_english as product_category_name
   from source
 )
 
