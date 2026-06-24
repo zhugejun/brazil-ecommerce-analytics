918d02debd782d4bb5ab6a53e91039dd3715d060
Gejun Zhu <gejun.anderson@gmail.com>
2026-06-23 22:33:18 -0500

    update column names

--- brazil_ecommerce_analytics/models/staging/stg_olist__category_name_translation.sql
+++ brazil_ecommerce_analytics/models/staging/stg_olist__category_name_translation.sql
@@ -1,11 +1,11 @@
 with source as (
-  select * from {{ source('olist', 'olist_category_name_translation') }}
+  select * from {{ source('olist', 'product_category_name_translation') }}
 ),
 
 renamed as (
   select
-    product_category_name as product_category_name_portuguese,
-    product_category_name_english as product_category_name
+    product_category_name as category_name_portuguese,
+    product_category_name_english as category_name
   from source
 )
 
